ad78c8659f14c6c91c3fce229dacf54446a0fc9a
tharun-kumar-r <rajtharunir@gmail.com>
2024-04-29 18:16:17 +0530

    v1

---
+++ charged.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace battery
+{
+    public partial class charged : Form
+    {
+        public charged()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.timer1.Enabled = true;
+            this.Close();
+
+
+        }
+    }
+}
